55814ef2090690f5de3b9af4c9f5308a96a76919
IMRANDIL <aliimranadil2@gmail.com>
2023-05-24 08:21:28 +0530

    added one more widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,14 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(207, 37, 28, 52),
-              Color.fromARGB(255, 62, 53, 77)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(207, 37, 28, 52),
+                Color.fromARGB(255, 62, 53, 77)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
           child: Center(
             child: Column(
@@ -20,21 +24,38 @@ void main() {
                   builder: (context, constraints) {
                     return Image.asset(
                       'assets/flutter.gif',
-                      width: constraints.maxWidth *
-                          0.4, // Adjust the width as needed
+                      width: constraints.maxWidth * 0.4,
                     );
                   },
                 ),
-                const SizedBox(
-                  height: 40,
-                ),
+                const SizedBox(height: 40),
                 const Text(
-                  'Learn Flutter the fun way!',
+                  'Learn Vocabulary the fun way!',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
                   ),
-                )
+                ),
+                const SizedBox(height: 30),
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.arrow_right_alt, color: Colors.white),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Start',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
