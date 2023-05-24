c7271bd3ea19239c38ad6e3079fc8863ccd1d4a7
IMRANDIL <aliimranadil2@gmail.com>
2023-05-24 07:52:58 +0530

    added some more style

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,38 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 113, 103, 103),
-        body: Center(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Image.asset(
-                'assets/flutter.gif',
-                width: constraints.maxWidth * 0.4, // Adjust the width as needed
-              );
-            },
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(207, 37, 28, 52),
+              Color.fromARGB(255, 62, 53, 77)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Image.asset(
+                      'assets/flutter.gif',
+                      width: constraints.maxWidth *
+                          0.4, // Adjust the width as needed
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
