9fa5e44a068b944e4024caa062d8081d9db16d64
IMRANDIL <aliimranadil2@gmail.com>
2023-05-24 11:05:14 +0530

    different way to render the screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,17 +10,17 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  // Widget? activeScreen;
+  var activeScreen = 'start-screen';
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'question-screen';
     });
   }
 
@@ -30,18 +30,19 @@ class _QuizState extends State<Quiz> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(207, 37, 28, 52),
-                Color.fromARGB(255, 62, 53, 77)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(207, 37, 28, 52),
+                  Color.fromARGB(255, 62, 53, 77)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
-          ),
-          child: activeScreen,
-        ),
+            child: activeScreen == 'start-screen'
+                ? StartScreen(switchScreen)
+                : const QuestionsScreen()),
       ),
     );
   }
