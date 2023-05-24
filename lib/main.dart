624536a6b29139438e8eeceabc6ec2fe15e336dc
IMRANDIL <aliimranadil2@gmail.com>
2023-05-24 09:06:09 +0530

    added some more refactoring done

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,23 @@ import 'package:flutter_second_app/StartScreen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: StartScreen()),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(207, 37, 28, 52),
+                Color.fromARGB(255, 62, 53, 77)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
     ),
   );
 }
