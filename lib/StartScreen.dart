7663d5f5532e33b70676de89f3fdd7d672e7fb36
IMRANDIL <aliimranadil2@gmail.com>
2023-05-24 08:51:02 +0530

    added refactoring

---
+++ lib/StartScreen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(207, 37, 28, 52),
+            Color.fromARGB(255, 62, 53, 77)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return Image.asset(
+                  'assets/flutter.gif',
+                  width: constraints.maxWidth * 0.4,
+                );
+              },
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'Learn Vocabulary the fun way!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.arrow_right_alt, color: Colors.white),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Start',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
