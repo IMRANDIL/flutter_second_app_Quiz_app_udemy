a3fd524fe65938735807337c057d8be8613e7841
IMRANDIL <aliimranadil2@gmail.com>
2023-05-24 07:32:00 +0530

    second app flutter quiz app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 113, 103, 103),
+        body: Center(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Image.asset(
+                'assets/flutter.gif',
+                width: constraints.maxWidth * 0.5, // Adjust the width as needed
+              );
+            },
+          ),
+        ),
+      ),
+    ),
+  );
+}
