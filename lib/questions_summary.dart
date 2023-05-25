b937a897907c234af12e0e2ca66c3b6bd1975b03
IMRANDIL <aliimranadil2@gmail.com>
2023-05-25 10:24:58 +0530

    added scroll to the question summary

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -6,34 +6,39 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map(
-        (data) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                ((data['question_index'] as int) + 1).toString(),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(data['current_question'] as String),
-                    const SizedBox(
-                      height: 5,
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    ((data['question_index'] as int) + 1).toString(),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(data['current_question'] as String),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(data['user_answer'] as String),
+                        Text(data['current_answer'] as String),
+                      ],
                     ),
-                    Text(data['user_answer'] as String),
-                    Text(data['current_answer'] as String),
-                  ],
-                ),
-              )
-            ],
-          );
-        },
-      ).toList(),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
