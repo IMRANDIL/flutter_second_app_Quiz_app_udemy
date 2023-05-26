cf10e9cc41b4da8d81f26efb723bc94f01da7bc6
IMRANDIL <aliimranadil2@gmail.com>
2023-05-26 07:32:40 +0530

    some more styles added

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -6,34 +6,62 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    // Calculate responsive dimensions based on screen size
+    final containerWidth = screenWidth * 0.8;
+    final containerHeight = screenHeight * 0.5;
+    final rowWidth = screenWidth * 0.9;
+
     return SizedBox(
-      height: 300,
+      height: containerHeight,
+      // color: Colors.green,
+      width: containerWidth,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
             (data) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    ((data['question_index'] as int) + 1).toString(),
+              return Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4),
                   ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(data['current_question'] as String),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(data['user_answer'] as String),
-                        Text(data['current_answer'] as String),
-                      ],
+                  color: Colors.red,
+                ),
+                width: rowWidth,
+                height: containerWidth * 0.3,
+                padding: EdgeInsets.all(rowWidth * 0.03),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      textAlign: TextAlign.center,
                     ),
-                  )
-                ],
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(data['current_question'] as String),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(data['user_answer'] as String),
+                          Text(data['current_answer'] as String),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               );
             },
           ).toList(),
